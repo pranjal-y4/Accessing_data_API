462f650354a020f44dc34e86b94f2c5a4c49970f
Pranjal Yadav <85802579+pranjal-y4@users.noreply.github.com>
2023-08-02 14:17:54 +0530

    Update youtube_analysis.py

--- youtube_analysis.py
+++ youtube_analysis.py
@@ -5,8 +5,8 @@ import seaborn as sns
 import csv
 
 
-api_key = 'AIzaSyBkUCK6I6TsCCYsHCCE2wseX8cv99MB424'
-channel_id = 'UCnz-ZXXER4jOvuED5trXfEA' #https://commentpicker.com/youtube-channel-id.php
+api_key = '--'
+channel_id = '--' #https://commentpicker.com/youtube-channel-id.php
 
 youtube =build('youtube','v3',developerKey = api_key) #.json file for 0auth
 
